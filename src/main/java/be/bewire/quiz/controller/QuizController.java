4d3ea41a4bbf565938ac3ad979fe6a23349a2420
Berten De Schutter <berten@gmail.com>
2022-08-11 22:58:26 +0200

    Clean Up Imports

--- src/main/java/be/bewire/quiz/controller/QuizController.java
+++ src/main/java/be/bewire/quiz/controller/QuizController.java
@@ -1,24 +1,14 @@
 package be.bewire.quiz.controller;
 
-import be.bewire.quiz.DTO.QuizDTO;
-import be.bewire.quiz.DTO.QuizDetailDTO;
 import be.bewire.quiz.model.Quiz;
 import be.bewire.quiz.repository.QuizRepository;
 import be.bewire.quiz.repository.entity.QuizEntity;
-import be.bewire.quiz.service.QuizService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dozer.DozerBeanMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
